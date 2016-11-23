08bc694441d4cc58653c72a81908a1a56a1abbaa
Ramon Malaquias <malaquias@imd.ufrn.br>
2016-11-23 10:58:29 -0300

    Criacao projeto maven

---
+++ ProjetoGCM/src/main/java/br/com/projetogcm/main/Main.java
@@ -0,0 +1,8 @@
+package br.com.projetogcm.main;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Olá mundo!");
+		System.out.println("Mais uma modificação.");
+	}
+}
